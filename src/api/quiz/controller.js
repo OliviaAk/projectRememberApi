938413f46b8b3e5fcc95d40f9ab5bf877eca05ba
Olivia <olivia.akhmaeva@gmail.com>
2021-05-12 17:02:40 +0300

    add edit and remove controllers

--- src/api/quiz/controller.js
+++ src/api/quiz/controller.js
@@ -21,9 +21,9 @@ const getQuestions = async (req, res) => {
 const createQuiz = async (req, res) => {
 	try {
 		const { quizName } = req.body;
-		
+
 		const newCard = new Quiz({
-			quizName
+			quizName,
 		});
 		const card = await newCard.save();
 		return res.status(201).json(card);
@@ -34,10 +34,13 @@ const createQuiz = async (req, res) => {
 
 const createQuestion = async (req, res) => {
 	try {
-		const { question,answers,correct,quizId } = req.body;
-		
+		const { question, answers, correct, quizId } = req.body;
+
 		const newCard = new Question({
-			question,answers,correct,quizId 
+			question,
+			answers,
+			correct,
+			quizId,
 		});
 		const card = await newCard.save();
 		return res.status(201).json(card);
@@ -46,9 +49,83 @@ const createQuestion = async (req, res) => {
 	}
 };
 
+const editQuiz = async (req, res) => {
+	try {
+		const _id = req.params.id;
+		const { quizName } = req.body;
+		if (_id) {
+			const formatId = String(_id);
+			const updatedEvent = await Quiz.updateOne(
+				{ _id: formatId },
+				{ $set: { quizName } },
+			);
+			if (updatedEvent) {
+				const event = await Quiz.findById({ _id: formatId });
+				return res.status(200).json(event);
+			}
+		}
+		res.sendStatus(404);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+};
+const deleteQuiz = async (req, res) => {
+	try {
+		const _id = req.params.id;
+		if (_id) {
+			const formatId = String(_id);
+			await Quiz.deleteOne({ _id: formatId });
+
+			return res.status(200).json(formatId);
+		}
+		res.sendStatus(404);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+};
+
+const editQuestions = async (req, res) => {
+	try {
+		const _id = req.params.id;
+		const { question, answers, correct } = req.body;
+		if (_id) {
+			const formatId = String(_id);
+			const updatedEvent = await Question.updateOne(
+				{ _id: formatId },
+				{ $set: { question, answers, correct } },
+			);
+			if (updatedEvent) {
+				const event = await Question.findById({ _id: formatId });
+				return res.status(200).json(event);
+			}
+		}
+		res.sendStatus(404);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+};
+const deleteQuestion = async (req, res) => {
+	try {
+		const _id = req.params.id;
+		if (_id) {
+			const formatId = String(_id);
+			await Question.deleteOne({ _id: formatId });
+
+			return res.status(200).json(formatId);
+		}
+		res.sendStatus(404);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+};
+
 module.exports = {
 	getQuiz,
 	createQuiz,
 	createQuestion,
-	getQuestions
+	getQuestions,
+	editQuiz,
+	deleteQuiz,
+	editQuestions,
+	deleteQuestion,
 };
--- src/api/quiz/routes.js
+++ src/api/quiz/routes.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const { getQuiz , createQuiz,createQuestion, getQuestions} = require("./controller");
+const {
+	getQuiz,
+	createQuiz,
+	createQuestion,
+	getQuestions,
+	editQuiz,
+	deleteQuiz,
+	editQuestions,
+	deleteQuestion,
+} = require("./controller");
 
 const route = express.Router();
 
 route.get("/", getQuiz);
-route.post('/',createQuiz);
-route.post('/questions', createQuestion)
-route.get('/questions', getQuestions)
-
+route.post("/", createQuiz);
+route.patch("/:id", editQuiz);
+route.delete("/:id", deleteQuiz);
+route.post("/questions", createQuestion);
+route.get("/questions", getQuestions);
+route.patch("/questions/:id", editQuestions);
+route.delete("/questions/:id", deleteQuestion);
 module.exports = route;
